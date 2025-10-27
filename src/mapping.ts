8707c8db55a87ac9d488baad8ba072d72055ce50
kriptoalsat-sudo <kriptoalsat@kriptoalsat.online>
2025-10-27 21:41:32 +0300

    Update mapping.ts

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,39 +5,45 @@ import {
   Burn as BurnEvent
 } from "../generated/UniswapV3Pool/UniswapV3Pool"
 
+// Handle Swap Event
 export function handleSwap(event: SwapEvent): void {
   let swap = new Swap(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
   swap.sender = event.params.sender
   swap.recipient = event.params.recipient
   swap.amount0 = event.params.amount0
   swap.amount1 = event.params.amount1
-  swap.sqrtPriceX96 = event.params.sqrtPriceX96
-  swap.liquidity = event.params.liquidity
   swap.tick = event.params.tick
+  swap.blockNumber = event.block.number
   swap.timestamp = event.block.timestamp
   swap.save()
 }
 
+// Handle Mint Event
 export function handleMint(event: MintEvent): void {
   let mint = new Mint(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
   mint.owner = event.params.owner
-  mint.tickLower = event.params.tickLower
-  mint.tickUpper = event.params.tickUpper
+  mint.sender = event.params.sender
   mint.amount = event.params.amount
   mint.amount0 = event.params.amount0
   mint.amount1 = event.params.amount1
+  mint.tickLower = event.params.tickLower
+  mint.tickUpper = event.params.tickUpper
+  mint.blockNumber = event.block.number
   mint.timestamp = event.block.timestamp
   mint.save()
 }
 
+// Handle Burn Event
 export function handleBurn(event: BurnEvent): void {
   let burn = new Burn(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
   burn.owner = event.params.owner
-  burn.tickLower = event.params.tickLower
-  burn.tickUpper = event.params.tickUpper
+  burn.sender = event.params.sender
   burn.amount = event.params.amount
   burn.amount0 = event.params.amount0
   burn.amount1 = event.params.amount1
+  burn.tickLower = event.params.tickLower
+  burn.tickUpper = event.params.tickUpper
+  burn.blockNumber = event.block.number
   burn.timestamp = event.block.timestamp
   burn.save()
 }
